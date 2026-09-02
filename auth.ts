7a64bea917f48c1945f5a441e9ca0c8963a0fa99
Credkellar-Boop <darnellwashingtonjr94@gmail.com>
2026-09-02 16:03:07 -0700

    Create auth.ts

---
+++ auth.ts
@@ -0,0 +1,22 @@
+import NextAuth from 'next-auth';
+import GitHub from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { db } from '@/lib/db';
+import { authConfig } from './auth.config';
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  ...authConfig,
+  adapter: PrismaAdapter(db),
+  session: { strategy: 'jwt' },
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    })
+  ],
+});
